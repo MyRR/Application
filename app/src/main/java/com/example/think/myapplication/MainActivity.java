bf075a76db84925629f7b7225cd37c7c4e6577ca
Mhy <yangminghui_net@126. com>
2016-05-29 15:19:34 +0800

    dd
    (cherry picked from commit a4e8bbc40b3e9ca38c76dfce79b22ec8e77e49c2)
    
    # Conflicts:
    #	app/src/main/java/com/example/think/myapplication/MainActivity.java

--- app/src/main/java/com/example/think/myapplication/MainActivity.java
+++ app/src/main/java/com/example/think/myapplication/MainActivity.java
@@ -6,15 +6,13 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button;
-
-
 
 
     }
